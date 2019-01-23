a7610d62adeb4f41a86b6799a36f8399e650b434
martin95056 <martinangelov056@gmail.com>
2019-01-23 23:49:39 +0200

    Add arrivals into saler report

--- accounting/selectors.py
+++ accounting/selectors.py
@@ -1,7 +1,10 @@
+from datetime import date
+
 from django.contrib.auth.models import User
 
 from locations.models import Location
 from accounting.models import Payment, Invoice, CreditNote, Refund
+from bookings.selectors import get_arrivals
 
 from permissions.services import check_entity_permission
 from permissions.constants import EntityConstants
@@ -19,11 +22,14 @@ def get_sales_report(*, location: Location, user: User) -> dict:
     payments = Payment.objects.filter(invoice__booking__location=location).values_list('amount', flat=True)
     refunds = Refund.objects.filter(credit_note__booking__location=location).values_list('amount', flat=True)
 
-    result = {
+    report = {
         'invoices': sum(invoices),
         'credit_notes': sum(credit_notes),
         'payments': sum(payments),
         'refunds': sum(refunds)
     }
 
-    return result
+    arrivals = get_arrivals(location=location, user=user)
+    today_arrivals = arrivals.get(str(date.today()), [])
+
+    return {'report': report, 'arrivals': sum(x['rent'] for x in today_arrivals)}
