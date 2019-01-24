9a2c9c5918c52849f1311028c1620d8d2f098a47
martin95056 <martinangelov056@gmail.com>
2019-01-24 10:02:23 +0200

    Duplicate get_arrivals to skip permission checks

--- accounting/selectors.py
+++ accounting/selectors.py
@@ -1,10 +1,10 @@
-from datetime import date
+from datetime import date, timedelta
 
 from django.contrib.auth.models import User
 
 from locations.models import Location
 from accounting.models import Payment, Invoice, CreditNote, Refund
-from bookings.selectors import get_arrivals
+from bookings.models import Booking
 
 from permissions.services import check_entity_permission
 from permissions.constants import EntityConstants
@@ -29,7 +29,33 @@ def get_sales_report(*, location: Location, user: User) -> dict:
         'refunds': sum(refunds)
     }
 
-    arrivals = get_arrivals(location=location, user=user)
+    arrivals = get_arrivals_for_sales_report(location=location)
     today_arrivals = arrivals.get(str(date.today()), [])
 
     return {'report': report, 'arrivals': sum(x['rent'] for x in today_arrivals)}
+
+
+def get_arrivals_for_sales_report(*, location: Location) -> dict:
+    """
+    This selector is exactly the same as `bookings.selectors.get_arrivals`
+    but doesn't perform permission checks.
+    """
+    arrival_dates = [
+        date.today() - timedelta(days=1),
+        date.today(),
+        date.today() + timedelta(days=1)
+    ]
+
+    arrivals = Booking.objects.filter(created_at__in=arrival_dates, location=location)
+    # Initializing the result
+    result = {str(date): [] for date in arrival_dates}
+    # Building the result
+    for date_ in arrival_dates:
+        for arrival in arrivals:
+            if arrival.created_at == date_:
+                result[str(date_)].append({
+                    'id': arrival.id,
+                    'rent': arrival.rent
+                })
+
+    return result
