d59b06af661d8a61e8a8eb723466c00d973d2d41
martin95056 <martinangelov056@gmail.com>
2019-01-30 16:55:57 +0200

    Move stack logic into `get_caller` util

--- permissions/services.py
+++ permissions/services.py
@@ -4,7 +4,7 @@ from locations.models import Location
 
 from permissions.constants import Entity
 from permissions.exceptions import NoEntityPermissionException
-from permissions.utils import Caller
+from permissions.utils import get_caller
 
 
 def check_entity_permission(
@@ -13,19 +13,9 @@ def check_entity_permission(
     entity: Entity,
     location: Location,
 ) -> None:
-    import inspect
+    caller = get_caller()
 
-    stack = inspect.stack()
-    # Here I am in the `__call__` of the instance
-    caller_call_frame = stack[2].frame
-    # This is the Caller instance that called `check_entity_permission`
-    # *NOTE* If the funciton is not wrapped with `with_optional_permissions` the `caller_itself`
-    # will not be a `Caller` instance (it'll most likely be the API that calls the selector)
-    caller_itself = caller_call_frame.f_locals.get('self', None)
-
-    print(f'The caller of the function: {caller_itself}')
-
-    if isinstance(caller_itself, Caller) and caller_itself._skip_permission_checks:
+    if getattr(caller, '_skip_permission_checks', False):
         return
 
     if user.is_superuser:
--- permissions/utils.py
+++ permissions/utils.py
@@ -14,3 +14,22 @@ def with_optional_permissions(func):
     caller = Caller(func)
 
     return caller
+
+
+def get_caller():
+    import inspect
+
+    stack = inspect.stack()
+    # Here I am in the `__call__` of the instance
+    caller_call_frame = stack[3].frame
+    # This is the Caller instance that called `check_entity_permission`
+    # *NOTE* If the funciton is not wrapped with `with_optional_permissions` the `caller_itself`
+    # will not be a `Caller` instance (it'll most likely be the API that calls the selector)
+    caller_itself = caller_call_frame.f_locals.get('self', None)
+
+    print(f'The caller of the function: {caller_itself}')
+
+    if not isinstance(caller_itself, Caller):
+        return None
+
+    return caller_itself
